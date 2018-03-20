be7797287998fba8f96cc899822cca0232bd2efe
Diego Ferraz <diego.ferraz>
2018-03-20 16:55:46 -0300

    Commit inicial com autenticacao de aplicacao de shoppign cart

---
+++ shopping-cart/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDC3hJs6MMqxuFU7-_h1I6wHz40JBM3jo",
+    authDomain: "angularshop-fb648.firebaseapp.com",
+    databaseURL: "https://angularshop-fb648.firebaseio.com",
+    projectId: "angularshop-fb648",
+    storageBucket: "",
+    messagingSenderId: "606412183585"
+  }
+};
---
+++ shopping-cart/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCDC3hJs6MMqxuFU7-_h1I6wHz40JBM3jo",
+    authDomain: "angularshop-fb648.firebaseapp.com",
+    databaseURL: "https://angularshop-fb648.firebaseio.com",
+    projectId: "angularshop-fb648",
+    storageBucket: "",
+    messagingSenderId: "606412183585"
+  }
+};
